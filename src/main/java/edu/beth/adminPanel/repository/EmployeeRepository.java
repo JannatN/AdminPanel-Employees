e4a2c93d23dd0484ec16a3d791ed8801bf15a088
JannatN <jannatfaisalnatsheh@gmail.com>
2021-05-20 22:40:33 +0300

    Last updates

--- src/main/java/edu/beth/adminPanel/repository/EmployeeRepository.java
+++ src/main/java/edu/beth/adminPanel/repository/EmployeeRepository.java
@@ -8,12 +8,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import edu.beth.adminPanel.entity.Employees;
 
 public interface EmployeeRepository extends JpaRepository<Employees, Integer> {
-	// @Query("SELECT d FROM Departments d WHERE CONCAT(d.dept_no, ' ', d.dept_name)
-	// LIKE %?1%") concat(CC.FirstName , ' ' , CC.LastName) LIKE 'n%'
-	@Query("SELECT e FROM Employees e WHERE e.lastName LIKE %?1%")
-//	@Query("SELECT e FROM Employees e WHERE e.firstName LIKE %?1%"
-//	+ " OR e.lastName LIKE %?1%"
-//	+ " OR CONCAT(e.firstName, '',e.lastName) LIKE %?1%")
+//	@Query("SELECT e FROM Employees e WHERE e.lastName LIKE %?1%"
+	@Query("SELECT e FROM Employees e WHERE e.firstName LIKE %?1%")
 	public List<Employees> findByFirstName(String keyword);
 
 }
--- src/main/java/edu/beth/adminPanel/service/EmployeeService.java
+++ src/main/java/edu/beth/adminPanel/service/EmployeeService.java
@@ -47,39 +47,5 @@ public class EmployeeService {
 		this.repository.deleteById(emp_no);
 	}
 
-//	public List<Employee> listAll(String keyword) {
-//		if (keyword != null) {
-//			return repository.search(keyword);
-//		}
-//		return repository.findAll();
-//	}
-
-//	public List<Employees> listAll(String keyword) {
-//		if (keyword != null) {
-//			return repository.search(keyword);
-//		}
-//		return repository.findAll();
-//	}
-
-//    public Product getProductById(int id) {
-//        return repository.findById(id).orElse(null);
-//    }
-//
-//    public Product getProductByName(String name) {
-//        return repository.findByName(name);
-//    }
-//
-//    public String deleteProduct(int id) {
-//        repository.deleteById(id);
-//        return "product removed !! " + id;
-//    }
-//
-////    public Product updateProduct(Product product) {
-////        Product existingProduct = repository.findById(product.getId()).orElse(null);
-////        existingProduct.setName(product.getName());
-////        existingProduct.setQuantity(product.getQuantity());
-////        existingProduct.setPrice(product.getPrice());
-////        return repository.save(existingProduct);
-////    }
 
 }
